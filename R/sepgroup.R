fb5428805ef3958818625263831e5b60dcd3168c
moyunie <17713543862@163.com>
2025-05-30 00:07:45 +0800

    add Sepgroup func

---
+++ R/sepgroup.R
@@ -0,0 +1,26 @@
+#' Title
+#'
+#' @param x
+#' @param threshold
+#'
+#' @returns generate group according to threshold
+#' @export
+#'
+#' @examples
+Sepgroup <- function(x,threshold){
+  if(length(x)==1){return(x)}
+  group = c()
+  x0 = x
+  i = 1
+  while(i <= length(x)){
+    diff = x[i]-x
+    loc = which(abs(diff) <= threshold,x)
+    set = which(x0 %in% unique(x[loc]))
+    #set = match(x[loc],x0)
+    group = append(group,list(set))
+    x = x[-loc]
+    if(i==0){break}
+  }
+  return(group)
+}
+
